34c87718a381187dc5b8e4500d7f240e5d159ac6
esnoComputas <esno@computas.com>
2024-04-05 19:33:02 +0200

    Unsafe implementation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+#![feature(core_intrinsics, ptr_internals)]
 use fastrand::{self, usize};
+use std::intrinsics::atomic_xchg_acqrel;
 use std::iter::repeat_with;
+use std::ptr;
+use std::thread::spawn;
 use std::{
     sync::atomic::{AtomicI8, Ordering},
     thread,
@@ -92,22 +96,28 @@ fn metropolis_step_atomic(lattice: &mut Array2<AtomicI8>, beta: f64) {
 fn metropolis_step(lattice: &mut Array2<i8>, beta: f64) {
     let n = lattice.shape()[0];
     let m = lattice.shape()[1];
-
-    let x_values = repeat_with(|| fastrand::Rng::new().usize(0..n));
-    let y_values = repeat_with(|| fastrand::Rng::new().usize(0..n));
-    let r_values = repeat_with(|| fastrand::Rng::new().f64());
-
-    for (x, y, r) in izip!(x_values, y_values, r_values).take(m * n) {
-        let right = lattice[[(x + 1) % n, y]];
-        let down = lattice[[x, (y + 1) % m]];
-        let left = lattice[[x.saturating_sub(1) % n, y]];
-        let up = lattice[[x, y.saturating_sub(1) % m]];
-        let curr = lattice[[x, y]];
-        let neighbors_sum = right + down + left + up;
-        let delta_e = (2 * curr * neighbors_sum) as f64;
-        if delta_e < 0_f64 || r < (-beta * delta_e).exp() {
-            lattice[[x, y]] *= -1;
-        }
+    let threads = std::thread::available_parallelism().unwrap().get();
+    let mut pool = Vec::with_capacity(threads);
+    for _ in 0..threads - 1 {
+        let data_ptr = ptr::Unique::new(lattice.as_mut_ptr());
+        pool.push(spawn(move || unsafe {
+            let latt = data_ptr.unwrap().as_ptr();
+            let x_values = repeat_with(|| fastrand::Rng::new().usize(0..n));
+            let y_values = repeat_with(|| fastrand::Rng::new().usize(0..n));
+            let r_values = repeat_with(|| fastrand::Rng::new().f64());
+            for (x, y, r) in izip!(x_values, y_values, r_values).take(m * n) {
+                let curr = latt.add(x + (m * y));
+                let right = latt.add(((x + 1) % n) + (m * y));
+                let down = latt.add(x + (m * ((y + 1) % m)));
+                let left = latt.add((x.saturating_sub(1) % n) + (m * y));
+                let up = latt.add(x + (m * (y.saturating_sub(1) % m)));
+                let neighbors_sum = *right + *down + *left + *up;
+                let delta_e = (2 * *curr * neighbors_sum) as f64;
+                if delta_e < 0_f64 || r < (-beta * delta_e).exp() {
+                    atomic_xchg_acqrel(curr, *curr * -1);
+                }
+            }
+        }))
     }
 }
 
