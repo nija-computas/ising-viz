3a0d447bee17bf657764e9fc165ca6dd020832ca
esnoComputas <esno@computas.com>
2024-04-05 18:31:29 +0200

    Fixed the thing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
-use std::{sync::atomic::{AtomicI8, Ordering}, thread, time::Instant};
+use fastrand::{self, usize};
+use std::iter::repeat_with;
+use std::{
+    sync::atomic::{AtomicI8, Ordering},
+    thread,
+    time::Instant,
+};
 
-use nannou::{image::{DynamicImage, ImageBuffer, Luma}, prelude::*};
+use nannou::{
+    image::{DynamicImage, ImageBuffer, Luma},
+    prelude::*,
+};
 
 use rand::distributions::{Distribution, Uniform};
 
 use ndarray::Array2;
 use ndarray_rand::RandomExt;
-
 
 use itertools::izip;
 use rayon::prelude::*;
@@ -18,7 +26,13 @@ fn initialize_spin_lattice(dim: (usize, usize)) -> Array2<i8> {
 }
 
 fn initialize_spin_lattice_atomic(dim: (usize, usize)) -> Array2<AtomicI8> {
-    Array2::random(dim, Uniform::new(0., 1.)).mapv(|a| if a > 0.5 { AtomicI8::new(1) } else { AtomicI8::new(-1) })
+    Array2::random(dim, Uniform::new(0., 1.)).mapv(|a| {
+        if a > 0.5 {
+            AtomicI8::new(1)
+        } else {
+            AtomicI8::new(-1)
+        }
+    })
 }
 
 // fn metropolis_step_parallel(lattice: &mut Array2<AtomicI8>, beta: f64) {
@@ -79,9 +93,9 @@ fn metropolis_step(lattice: &mut Array2<i8>, beta: f64) {
     let n = lattice.shape()[0];
     let m = lattice.shape()[1];
 
-    let x_values = Uniform::new(0, n).sample_iter(rand::thread_rng());
-    let y_values = Uniform::new(0, m).sample_iter(rand::thread_rng());
-    let r_values = Uniform::new(0., 1.).sample_iter(rand::thread_rng());
+    let x_values = repeat_with(|| fastrand::Rng::new().usize(0..n));
+    let y_values = repeat_with(|| fastrand::Rng::new().usize(0..n));
+    let r_values = repeat_with(|| fastrand::Rng::new().f64());
 
     for (x, y, r) in izip!(x_values, y_values, r_values).take(m * n) {
         let right = lattice[[(x + 1) % n, y]];
@@ -98,40 +112,44 @@ fn metropolis_step(lattice: &mut Array2<i8>, beta: f64) {
 }
 
 fn main() {
-    nannou::app(model)
-        .update(update)
-        .simple_window(view)
-        .run();
+    nannou::app(model).update(update).simple_window(view).run();
 }
 
 struct Model {
-    lattice: Array2<AtomicI8>
+    lattice: Array2<i8>,
 }
 
 fn model(_app: &App) -> Model {
-    _app.new_window().size(N as u32, N as u32).view(view).build().unwrap();
     Model {
-        lattice: initialize_spin_lattice_atomic((N, N))
+        lattice: initialize_spin_lattice((N, N)),
     }
 }
 
 fn update(_app: &App, _model: &mut Model, _update: Update) {
-    let beta = (_app.mouse.x as f64+((N/2) as f64))/((N/2) as f64);
+    let beta = (_app.mouse.x as f64 + ((N / 2) as f64)) / ((N / 2) as f64);
     let start = Instant::now();
-    metropolis_step_atomic(&mut _model.lattice, beta);
+    metropolis_step(&mut _model.lattice, beta);
     let duration = start.elapsed();
     print!("\rTime elapsed in iteration is: {:?}", duration);
 }
 
-fn view(_app: &App, _model: &Model, frame: Frame){
+fn view(_app: &App, _model: &Model, frame: Frame) {
     let draw = _app.draw();
-    let vec: Vec<u8> = _model.lattice.iter().map(|x| u8::try_from(255*(x.load(Ordering::Relaxed) as i16 + 1)/2).unwrap()).collect();
+    let vec: Vec<u8> = _model
+        .lattice
+        .iter()
+        .map(|x| u8::try_from(255 * (*x as i16 + 1) / 2).unwrap())
+        .collect();
     // let vec: Vec<u8> = _model.lattice.clone().into_raw_vec().iter().map(|x| u8::try_from(255*(*x as i16 + 1)/2).unwrap()).collect();
-    let image: ImageBuffer<Luma<u8>, Vec<u8>> = ImageBuffer::from_raw(_model.lattice.shape()[0] as u32, _model.lattice.shape()[1] as u32, vec).unwrap();
+    let image: ImageBuffer<Luma<u8>, Vec<u8>> = ImageBuffer::from_raw(
+        _model.lattice.shape()[0] as u32,
+        _model.lattice.shape()[1] as u32,
+        vec,
+    )
+    .unwrap();
     let dynamic_image: DynamicImage = DynamicImage::ImageLuma8(image);
     let texture = wgpu::Texture::from_image(_app, &dynamic_image);
     draw.background().color(BLACK);
     draw.texture(&texture);
     draw.to_frame(_app, &frame).unwrap();
 }
-
