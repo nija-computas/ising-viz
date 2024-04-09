f94276dc31d51929d9d137960a6a469efd5aa9fd
esnoComputas <esno@computas.com>
2024-04-10 00:08:09 +0200

    removed unsafe

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use std::iter::repeat_with;
+use std::{
+    iter::repeat_with,
+    sync::{
+        atomic::{AtomicI8, Ordering},
+        Arc,
+    },
+};
 use threadpool::ThreadPool;
 
 use nannou::{
@@ -13,39 +19,35 @@ use ndarray_rand::RandomExt;
 
 use itertools::izip;
 
-const N: usize = 2000;
+const N: usize = 1500;
 
-fn initialize_spin_lattice(dim: (usize, usize)) -> Array2<i8> {
-    Array2::random(dim, Uniform::new(0., 1.)).mapv(|a| if a > 0.5 { 1 } else { -1 })
+fn initialize_spin_lattice(dim: (usize, usize)) -> Array2<AtomicI8> {
+    Array2::random(dim, Uniform::new(0., 1.))
+        .mapv(|a| if a > 0.5 { 1 } else { -1 })
+        .mapv(|a| AtomicI8::new(a))
 }
-struct HoldsRawPtr {
-    ptr: *mut i8,
-}
-unsafe impl Send for HoldsRawPtr {}
-// Sync trait tells compiler that `&T` is safe to share between threads
-unsafe impl Sync for HoldsRawPtr {}
 
-fn metropolis_step(latt: HoldsRawPtr, beta: f64) {
-    let n = N;
-    let m = N;
-    let latt = latt.ptr;
-    unsafe {
-        let x_values = repeat_with(|| fastrand::Rng::new().usize(0..n));
-        let y_values = repeat_with(|| fastrand::Rng::new().usize(0..n));
-        let r_values = repeat_with(|| fastrand::Rng::new().f64());
-        for (x, y, r) in izip!(x_values, y_values, r_values).take(m * n) {
-            let curr = latt.add(x + (m * y));
-            let right = latt.add(((x + 1) % n) + (m * y));
-            let down = latt.add(x + (m * ((y + 1) % m)));
-            let left = latt.add((x.saturating_sub(1) % n) + (m * y));
-            let up = latt.add(x + (m * (y.saturating_sub(1) % m)));
-            let neighbors_sum = *right + *down + *left + *up;
-            let delta_e = (2 * *curr * neighbors_sum) as f64;
-            if delta_e < 0_f64 || r < (-beta * delta_e).exp() {
-                *curr = *curr * -1;
-            }
+fn metropolis_step(lattice: &mut Arc<Array2<AtomicI8>>, beta: f64) {
+    let n = lattice.shape()[0];
+    let m = lattice.shape()[1];
+
+    let x_values = repeat_with(|| fastrand::Rng::new().usize(0..n));
+    let y_values = repeat_with(|| fastrand::Rng::new().usize(0..m));
+    let r_values = repeat_with(|| fastrand::Rng::new().f64());
+
+    for (x, y, r) in izip!(x_values, y_values, r_values).take(m * n) {
+        let ordering = Ordering::Relaxed;
+        let right = lattice[[(x + 1) % n, y]].load(ordering);
+        let down = lattice[[x, (y + 1) % m]].load(ordering);
+        let left = lattice[[x.saturating_sub(1), y]].load(ordering);
+        let up = lattice[[x, y.saturating_sub(1)]].load(ordering);
+        let curr = &lattice[[x, y]];
+        let neighbors_sum = right + down + left + up;
+        let delta_e = (2 * curr.load(ordering) * neighbors_sum) as f64;
+        if delta_e < 0_f64 || r < (-beta * delta_e).exp() {
+            let _ = lattice[[x, y]].fetch_update(ordering, ordering, |v| Some(-v));
         }
-    };
+    }
 }
 
 fn main() {
@@ -54,13 +56,13 @@ fn main() {
 
 struct Model {
     pool: ThreadPool,
-    lattice: Array2<i8>,
+    lattice: Arc<Array2<AtomicI8>>,
 }
 
 fn model(_app: &App) -> Model {
     Model {
         pool: ThreadPool::new(std::thread::available_parallelism().unwrap().get()),
-        lattice: initialize_spin_lattice((N, N)),
+        lattice: Arc::new(initialize_spin_lattice((N, N))),
     }
 }
 
@@ -69,14 +71,10 @@ fn update(_app: &App, _model: &mut Model, _update: Update) {
         return;
     }
     let beta = (_app.mouse.x as f64 + ((N / 2) as f64)) / ((N / 2) as f64);
-    //let start = Instant::now();
-    let data_ptr = HoldsRawPtr {
-        ptr: (_model.lattice.as_mut_ptr()),
-    };
-    _model.pool.execute(move || metropolis_step(data_ptr, beta));
-
-    //let duration = start.elapsed();
-    //print!("\rTime elapsed in iteration is: {:?}", duration);
+    let mut lattice_ptr = Arc::clone(&_model.lattice);
+    _model
+        .pool
+        .execute(move || metropolis_step(&mut lattice_ptr, beta));
 }
 
 fn view(_app: &App, _model: &Model, frame: Frame) {
@@ -84,9 +82,8 @@ fn view(_app: &App, _model: &Model, frame: Frame) {
     let vec: Vec<u8> = _model
         .lattice
         .iter()
-        .map(|x| u8::try_from(255 * (*x as i16 + 1) / 2).unwrap())
+        .map(|x| u8::try_from(255 * (x.load(Ordering::Relaxed) as i16 + 1) / 2).unwrap())
         .collect();
-    // let vec: Vec<u8> = _model.lattice.clone().into_raw_vec().iter().map(|x| u8::try_from(255*(*x as i16 + 1)/2).unwrap()).collect();
     let image: ImageBuffer<Luma<u8>, Vec<u8>> = ImageBuffer::from_raw(
         _model.lattice.shape()[0] as u32,
         _model.lattice.shape()[1] as u32,
